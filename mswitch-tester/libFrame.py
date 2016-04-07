a4a57ffe1c91f94b395bbfca71fb18262a3ef70c
jzhengdj <jzhengdj@gmail.com>
2016-04-07 13:44:11 +0800

    Change window size to smaller

--- mswitch-tester/libFrame.py
+++ mswitch-tester/libFrame.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 
-w = 800
-h = 450
+w = 180
+h = 300
 
 class MicroSwitchTester:
     """A panel to test the status of MicroSwitch"""
@@ -14,8 +14,8 @@ class MicroSwitchTester:
         s.configure("My.TFrame", background = 'green')
         s.configure("My.TLabel", background = 'green')
 
-        frame = ttk.Frame(mainframe, borderwidth=5, relief="sunken", width=w, height=h, style = 'My.TFrame')
-        ttk.Label(mainframe, text='MicroSwitch Tester', font = "Verdana 40 bold italic", style = 'My.TLabel').grid(column=0, row=0, columnspan=3, rowspan=4, sticky = W)
+        frame = ttk.Frame(mainframe, borderwidth=5, width=w, height=h, style = 'My.TFrame')
+        ttk.Label(mainframe, text='MicroSwitch', font = "Verdana 18 bold italic", style = 'My.TLabel').grid(column=0, row=0, columnspan=3, rowspan=4, sticky = W)
         #c.create_text(text = 'test')
         # ttk.Button(mainframe, text="Green", command = self.changeGreen).grid(column=1, row=3, sticky=W)
         # ttk.Button(mainframe, text="Red", command = self.changeRed).grid(column=2, row=3, sticky=W)
@@ -45,7 +45,7 @@ class MicroSwitchTester:
         # hs = root.winfo_screenheight() # height of the screen
 
         # calculate x and y coordinates for the Tk root window
-        x = (ws/10)
+        x = 0
         y = (hs*9/10) - (h)
 
         # set the dimensions of the screen
